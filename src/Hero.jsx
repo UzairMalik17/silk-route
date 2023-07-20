bf318796fea47bde18f616aea3bc149174b95f9f
UzairMalik17 <uzairam17@gmail.com>
2023-07-20 23:18:52 +0500

    Hero section updated

---
+++ src/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Pin from "./images/Pin.png";
+import Riga from "./images/Riga.png";
+import Slider from "./images/Slider.png";
+function Hero() {
+  return (
+    <section className="bg-heroImage bg-cover bg-center bg-no-repeat w-full h-[calc(100vh+6rem)]">
+      <h1 className="font-anton text-white leading-[7.5rem] text-[5rem] absolute top-[13rem] left-[8.4375rem]">
+        Immigration
+        <br />
+        Tailored
+        <br />
+        to You
+      </h1>
+      <img
+        src={Pin}
+        alt="Riga"
+        className="w-[1.5625rem] h-[2.51875rem] absolute top-[21.875rem] left-[63.8125rem]"></img>
+      <img
+        src={Riga}
+        alt="Riga"
+        className="w-[1.75rem] h-[1.375rem] absolute top-[24.39375rem] left-[63.8125rem]"></img>
+      <img
+        src={Slider}
+        alt="Slider"
+        className="w-[68.875rem] h-[2.0625rem] absolute top-[42.934375rem] left-[10.6275rem]"></img>
+    </section>
+  );
+}
+
+export default Hero;
