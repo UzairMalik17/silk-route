c7df4e552ba20c23bf95ccffdec59648cffc3bbb
UzairMalik17 <uzairam17@gmail.com>
2023-07-22 18:52:04 +0500

    Footer responsive

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,11 +3,11 @@ import logo from "./images/LOGO.png";
 
 function Navbar() {
   return (
-    <nav className="bg-transparent  inline-flex fixed">
+    <nav className="bg-transparent flex fixed w-full">
       <div className="pt-2 pl-24 pb-1.5">
         <img src={logo} alt="Logo" className="h-[82px] w-[196.81px]" />
       </div>
-      <div className="text-white absolute top-0 left-[41.25rem] h-[5.8125rem] w-[40.3125rem] flex items-center justify-between font-sansNarrow">
+      <div className="text-white md:pl[15rem] lg:pl-[18rem] xl:pl-[22.25rem] md:pr-24 h-[5.8125rem] w-[calc(100vw-12.3rem)] flex items-center justify-between font-sansNarrow">
         <a href="#">Home</a>
         <a href="#">About Us</a>
         <a href="#">Services</a>
